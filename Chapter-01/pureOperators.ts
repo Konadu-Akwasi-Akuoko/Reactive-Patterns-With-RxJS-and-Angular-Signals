d6cd40692d0dbd3843b50160a538f168b22e26aa
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-09-05 15:34:21 +0000

    feat: rxjs operators are pure functions

---
+++ Chapter-01/pureOperators.ts
@@ -0,0 +1,21 @@
+/*
+ * This shows that all rxjs operators are pure funcitons
+ */
+
+import { map, of } from "rxjs";
+
+const source = of(1, 2, 3);
+const result = source.pipe(
+  map((value: number) => value + 1),
+  map((value: number) => value + 1),
+);
+
+source.subscribe({
+  next: (value) => {
+    console.log(value);
+  },
+});
+
+result.subscribe((value) => {
+  console.log(value);
+});
